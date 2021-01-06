d31ca73c47bb5f40e0e829fc44ef98d8f479db48
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:25:15 +0200

    Add files via upload

---
+++ Task01_Cinema/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string typeOfHall = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double sum = 0;
+            // math
+            int places = rows * colums;
+            // check and ouput
+            switch (typeOfHall)
+            {
+                case "Premiere":
+                    {
+                        sum = places * 12;
+                    }
+                    break;
+                case "Normal":
+                    {
+                        sum = places * 7.5;
+                    }
+                    break;
+                case "Discount":
+                    {
+                        sum = places * 5;
+                    }
+                    break;
+            }
+            Console.WriteLine($"{sum:F2} leva");
+        }
+    }
+}
