fed811bd5749491b8e958063c0e3a4a8cbc75153
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-06 20:27:20 +0200

    Add files via upload

---
+++ Task03_New_House/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Task03_New_House
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string kindOfFlower = Console.ReadLine();
+            int countOfFlowers = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double sum = 0;
+            // check and ouput
+            switch (kindOfFlower)
+            {
+                case "Roses":
+                    {
+                        sum = countOfFlowers * 5;
+                        if (countOfFlowers > 80)
+                        {
+                            sum -= (sum * 0.1);
+                        }
+                    }
+                    break;
+                case "Dahlias":
+                    {
+                        sum = countOfFlowers * 3.8;
+                        if (countOfFlowers > 90)
+                        {
+                            sum -= (sum * 0.15);
+                        }
+                    }
+                    break;
+                case "Tulips":
+                    {
+                        sum = countOfFlowers * 2.8;
+                        if (countOfFlowers > 80)
+                        {
+                            sum = sum - (sum * 0.15);
+                        }
+                    }
+                    break;
+                case "Narcissus":
+                    {
+                        sum = countOfFlowers * 3;
+                        if (countOfFlowers < 120)
+                        {
+                            sum = sum + (sum * 0.15);
+                        }
+                    }
+                    break;
+                case "Gladiolus":
+                    {
+                        sum = countOfFlowers * 2.5;
+                        if (countOfFlowers < 80)
+                        {
+                            sum = sum + (sum * 0.2);
+                        }
+                    }
+                    break;
+            }
+            if (sum < budget)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {countOfFlowers} {kindOfFlower} and {(budget-sum):F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {(sum - budget):F2} leva more.");
+            }
+        }
+    }
+}
